25d52f5e145c1fabe1c1b881d54b61388f0ea6b6
Rodonies <matthew@berkvens.com>
2015-10-11 00:14:31 +0200

    First REST API test in a console project

---
+++ Console_RestAPI/Console_RestAPI/Data/user.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_RestAPI
+{
+    public class Users
+    {
+        public User[] users { get; set; }
+    }
+
+    public class User
+    {
+        public _Id _id { get; set; }
+        public string username { get; set; }
+        public string hash { get; set; }
+        public string salt { get; set; }
+        public string mail { get; set; }
+        public Answered[] answered { get; set; }
+    }
+
+    public class _Id
+    {
+        public string oid { get; set; }
+    }
+
+    public class Answered
+    {
+        public string id { get; set; }
+        public bool[] answers { get; set; }
+    }
+}
---
+++ Console_RestAPI/Console_RestAPI/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestSharp;
+using System.Configuration;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace Console_RestAPI
+{
+    class Program
+    {
+        private static string KEY_API = "apikey";
+
+        static void Main(string[] args)
+        {
+            string apikey = ConfigurationManager.ConnectionStrings[KEY_API].ConnectionString;
+
+            var client = new RestClient("https://api.mongolab.com/api/1");
+            var request = new
+            RestRequest("databases/{database}/collections/{collection}/?apiKey={apikey}&q={'username':'admin'}", Method.GET);
+            request.AddParameter("apikey", apikey, ParameterType.UrlSegment);
+            request.AddParameter("database", "MongoLab_Mat", ParameterType.UrlSegment);
+            request.AddParameter("collection", "users", ParameterType.UrlSegment);
+            var res = client.Execute(request);
+            Users result = JsonConvert.DeserializeObject<Users>("{ 'users': " + res.Content + "}");
+            Console.WriteLine(res.Content);
+            Console.ReadLine();
+        }
+    }
+}
