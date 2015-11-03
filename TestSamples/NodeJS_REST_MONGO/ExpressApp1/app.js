542a6075b9f8382ffdd1c57baf49b863ec5136d3
Rodonies <matthew@berkvens.com>
2015-11-03 01:44:49 +0100

    Website is now using self-signed certificates

--- TestSamples/NodeJS_REST_MONGO/ExpressApp1/app.js
+++ TestSamples/NodeJS_REST_MONGO/ExpressApp1/app.js
@@ -1,11 +1,26 @@
 ﻿var express = require('express');
 var path = require('path');
+var https = require('https');
+var fs = require('fs');
 var favicon = require('serve-favicon');
 var bodyParser = require('body-parser');
 
-//DB + Mongoose
+//Mongoose
 var mongoose = require('./mongoose/dbconnection');
-var schemas = require("./mongoose/schemas");
+var schemas = require('./mongoose/schemas');
+
+//Certificate
+var sslOptions = {
+    key: fs.readFileSync('./ssl/server.key'),
+    cert: fs.readFileSync('./ssl/server.crt'),
+    ca: fs.readFileSync('./ssl/ca.crt'),
+    requestCert: true,
+    rejectUnauthorized: false
+};
+
+//Session & Auth
+var session = require('express-session');
+var sess;
 
 //routes
 var indexRoutes = require('./routes/index');
@@ -22,11 +37,29 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: "shhhhhitsasecret",
+    name: "Codegenie",
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use('/', indexRoutes);
 app.use('/users/', userRoutes);
 app.use('/exercises', exerciseRoutes);
 app.use('/exercises', answerRoutes);
 
-app.listen(app.get('port'));
+
+
+https.createServer(sslOptions, app).listen(2000);
+
+
+
+var http = express();
+http.get('*', function (req, res) {
+    res.redirect('https://localhost:2000' + req.url)
+}).listen(1337);
+
+
+
 console.log('Express server started on port %s', app.get('port'));
