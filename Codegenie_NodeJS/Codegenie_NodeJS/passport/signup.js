df648b6acba852223a004321502f3df51d600a79
ErazerBrecht <brecht.carlier@student.ap.be>
2015-11-04 00:19:40 +0100

    Made it back possible to add new users

--- Codegenie_NodeJS/Codegenie_NodeJS/passport/signup.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/passport/signup.js
@@ -9,44 +9,45 @@ module.exports = function (passport) {
         passReqToCallback: true
     },
         function (req, username, password, done) {
-            console.log("passport signup called");
-
-            findOrCreateUser = function () {
-                UserModel.findOne({ 'name': username }, function (err, user) {
-                    if (err) {
-                        console.log('Error in SignUp: ' + err);
-                        return done(null, false, err);
-                    }
-                    if (user) {
-                        console.log('User already exists');
-                        return done(null, false, req.flash('message', 'User Already Exists'));
-                    } else {
-                        var newUser = new UserModel({
-                            name: username,
-                            password: createHash(password),
-                            class: req.param('class'),
-                            status: 0,
-                            admin: false,
-                            registerdate: moment().format("DD/MM/YYYY"),
-                            lastseen: moment().format("DD/MM/YYYY")
-                        });
-
-                        newUser.save(function (err) {
-                            if (err) {
-                                console.log('Error in Saving user: ' + err);
-                                done(null, false, err);
-                            }
-                            return done(null, newUser);
-                        });
-                        console.log('Signup:' + newUser.name);
-                    }
-                });
-            };
-
-            process.nextTick(findOrCreateUser);
-        })
+        console.log("passport signup called");
+        
+        findOrCreateUser = function () {
+            UserModel.findOne({ 'name': username }, function (err, user) {
+                if (err) {
+                    console.log('Error in SignUp: ' + err);
+                    return done(null, false, err);
+                }
+                if (user) {
+                    console.log('User already exists');
+                    return done(null, false, req.flash('message', 'User Already Exists'));
+                } else {
+                    var newUser = new UserModel({
+                        name: username,
+                        password: createHash(password),
+                        class: req.body.group,
+                        email: req.body.email,
+                        status: 0,
+                        admin: false,
+                        registerdate: moment().format("DD/MM/YYYY"),
+                        lastseen: moment().format("DD/MM/YYYY")
+                    });
+                    
+                    newUser.save(function (err) {
+                        if (err) {
+                            console.log('Error in Saving user: ' + err);
+                            done(null, false, err);
+                        }
+                        return done(null, newUser);
+                    });
+                    console.log('Signup:' + newUser.name);
+                }
+            });
+        };
+        
+        process.nextTick(findOrCreateUser);
+    })
     );
-
+    
     var createHash = function (password) {
         return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
     }
