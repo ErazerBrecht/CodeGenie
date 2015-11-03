18363458dafc6f8a373c57ca15ed1fc51d8a3df4
ErazerBrecht <brecht.carlier@student.ap.be>
2015-11-03 12:01:27 +0100

    Added testsample for Node Passport

---
+++ TestSamples/NodeJS_Passport/ExpressApp1/passport/config.js
@@ -0,0 +1,27 @@
+﻿var login = require('./login');
+
+// Load up the user model
+var schemas = require("../mongoose/schemas");
+var UserModel = schemas.UserModel;
+
+// Expose this function to our app using module.exports
+module.exports = function (passport) {
+    
+    // Passport needs to be able to serialize and deserialize users to support persistent login sessions
+    passport.serializeUser(function (user, done) {
+        console.log('serializing user: '); console.log(user);
+        done(null, user._id);
+    });
+    
+    passport.deserializeUser(function (id, done) {
+        UserModel.findById(id, function (err, user) {
+            console.log('deserializing user:', user);
+            done(err, user);
+        });
+    });
+    
+    // Setting up Passport Strategies for Login and SignUp/Registration
+    login(passport);
+    //TODO ADD SIGNUP
+    //signup(passport);
+}
