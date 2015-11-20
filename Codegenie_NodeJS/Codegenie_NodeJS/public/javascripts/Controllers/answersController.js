8bfe6bf0a9a6c45a20ed409b3b4f10f427e49e98
ErazerBrecht <brecht.carlier@student.ap.be>
2015-11-20 18:01:42 +0100

    Fixed bug in for loop Changed to angular's foreach!!!

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/answersController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/answersController.js
@@ -2,32 +2,21 @@
     
     var app = angular.module("adminApp");
     
-
+    
     var answersController = function ($scope, restData, $routeParams) {
-        var userData;
-        var len = 0;
         restData.getAllAnswers.query(function (data) {
             $scope.answers = data;
-           
-          
-            len = $scope.answers.length;
-
-            $scope.yolo();
-            
+            $scope.addUserNames();
         });
-
-        $scope.yolo = function () {
-            for (i = 0; i < len; i++) {
-                alert($scope.answers[i].userid);
-                restData.getUserById.get({ userid: $scope.answers[i].userid }, function (data) {
-                    $scope.answers[i].name = data.name;      
-
+        
+        $scope.addUserNames = function () {
+            angular.forEach($scope.answers, function (answer) {
+                restData.getUserById.get({ userid: answer.userid }, function (data) {
+                    answer.name = data.name;
                 });
-            }
+
+            });
         };
-        
-        
-        
-    };
+    }
     app.controller("answersController", answersController);
 }());
