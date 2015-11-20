c2fa07dec7cba5eacc40823d7696f0a3bc3131c5
ErazerBrecht <brecht.carlier@student.ap.be>
2015-11-20 21:24:03 +0100

    Added AJAX in Angular style @ExerciseController

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/exerciseController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/exerciseController.js
@@ -1,27 +1,45 @@
 ﻿(function () {
-	
+    
     var app = angular.module("adminApp");
-
-    var exerciseController = function ($scope, restData, $routeParams) {
-        $scope.questions = [];
-
-        $scope.ctor = function() {
-            $scope.addButton();
-        };
-        
+    
+    var exerciseController = function ($scope, $http, restData, $routeParams) {        
         //Add new question table into the form
-        $scope.addButton = function() {
+        $scope.addButton = function () {
             var question = {};
-            $scope.questions.push(question);
+            if ($scope.exercise == null)
+                $scope.exercise = {}
+            if ($scope.exercise.questions == null)
+                $scope.exercise.questions = [];
+
+            $scope.exercise.questions.push(question);
         };
         
         //Remove correct question table from the form
         $scope.removeButton = function (id) {
-            $scope.questions.splice(id, 1);
+            $scope.exercise.questions.splice(id, 1);
+            if ($scope.exercise.questions.length < 1) {
+                delete $scope.exercise.questions;
+            }
         };
-
-        $scope.ctor();
+        
+        $scope.processForm = function () {
+            $http({
+                method  : 'POST',
+                url     : '/admin/exercises/post',
+                data    : $scope.exercise, // pass in data as strings   
+                responseType: 'text'        
+            }).then(
+                //SUCCESS
+                function(response) {
+                    alert(response.data);
+                },
+                //ERROR
+                function(error) {
+                    alert(error.data);
+                }
+            );
+        };
     };
-	
-	app.controller("exerciseController", exerciseController);
+    
+    app.controller("exerciseController", exerciseController);
 })();
