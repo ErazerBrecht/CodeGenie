d85d95a3c867e94c079528618dff89a89cab3817
ErazerBrecht <brecht.carlier@student.ap.be>
2015-11-22 16:33:29 +0100

    Made able to edit existing exercises

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/ExercisesController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/ExercisesController.js
@@ -6,6 +6,28 @@
         restData.getExercises.query(function (data) {
             $scope.exercises = data;
         });
+
+        $scope.select = function(id) {
+            $scope.selected = $scope.exercises[id];
+        }
+
+        $scope.processForm = function () {
+            $http({
+                method  : 'POST',
+                url     : '/admin/exercises/edit/' + $scope.selected._id,
+                data    : $scope.selected,
+                responseType: 'text'
+            }).then(
+                //SUCCESS
+                function (response) {
+                    alert(response.data);
+                },
+                //ERROR
+                function (error) {
+                    alert(error.data);
+                }
+            );
+        };
     };
     
     app.controller("exercisesController", exercisesController);
