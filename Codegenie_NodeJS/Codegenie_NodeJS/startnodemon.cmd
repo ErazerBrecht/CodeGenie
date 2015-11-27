d9b498ffd7df9dff011e4e6796477ccfe8c381ce
ErazerBrecht <brecht.carlier@student.ap.be>
2015-11-27 23:01:34 +0100

    Little bug in startup script

--- Codegenie_NodeJS/Codegenie_NodeJS/startnodemon.cmd
+++ Codegenie_NodeJS/Codegenie_NodeJS/startnodemon.cmd
@@ -1,4 +1,8 @@
-set /p connection =< mongoose/connectioninfo.config
+rem Place a file with the name connctioninfo.config in the mongoose folder to connect to the database
+rem In this file there has to be the connectionstring (mongodb://....)
+rem Or change the path! Or hardcode it here...
+
+set /p connection=<mongoose/connectioninfo.config
 
 IF '%connection%' == '' set connection=connectionstring
 
