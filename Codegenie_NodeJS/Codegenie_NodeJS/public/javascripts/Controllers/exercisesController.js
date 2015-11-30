da3bb31ff0cdc06057f22f16bb646da865493932
ErazerBrecht <brecht.carlier@student.ap.be>
2015-11-30 21:52:34 +0100

    Exercises can now be deleted from db Added some gitignore stuff for other ide than vs

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/exercisesController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/exercisesController.js
@@ -16,15 +16,36 @@
         $scope.toggleCenterAnchor = function () { $scope.centerAnchor = !$scope.centerAnchor }
         $scope.onDropComplete = function (data) {
             var id = $scope.exercises.indexOf(data);
+            $scope.selected = $scope.exercises[id];
+            $scope.DeleteExercise();
             $scope.exercises.splice(id, 1);
-            //TODO: Remove exercise in DB with an ajax call
+            $scope.selected = null;
         }   
         
-        //AJAX Call
+        //AJAX Call POST
         $scope.processForm = function () {
             $http({
                 method  : 'POST',
                 url     : '/admin/exercises/edit/' + $scope.selected._id,
+                data    : $scope.selected,
+                responseType: 'text'
+            }).then(
+                //SUCCESS
+                function (response) {
+                    alert(response.data);
+                },
+                //ERROR
+                function (error) {
+                    alert(error.data);
+                }
+            );
+        };
+        
+        //AJAX Call Delete
+        $scope.DeleteExercise = function () {
+            $http({
+                method  : 'GET',
+                url     : '/admin/exercises/delete/' + $scope.selected._id,
                 data    : $scope.selected,
                 responseType: 'text'
             }).then(
