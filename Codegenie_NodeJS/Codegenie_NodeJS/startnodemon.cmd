46a1a8661885142fb849c6eedab691333abd49d0
Matthew Berkvens <matthew@berkvens.com>
2015-11-30 10:57:15 +0100

    removed spam on cmd startup

--- Codegenie_NodeJS/Codegenie_NodeJS/startnodemon.cmd
+++ Codegenie_NodeJS/Codegenie_NodeJS/startnodemon.cmd
@@ -1,3 +1,4 @@
+echo off
 rem Place a file with the name connctioninfo.config in the mongoose folder to connect to the database
 rem In this file there has to be the connectionstring (mongodb://....)
 rem Or change the path! Or hardcode it here...
