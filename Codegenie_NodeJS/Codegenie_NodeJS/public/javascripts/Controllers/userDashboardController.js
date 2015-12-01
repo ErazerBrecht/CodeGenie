496b4104350f14243fc48755dc1128b911a52814
ArneSchoonvliet <schoonvlietarne3@gmail.com>
2015-12-01 10:06:37 +0100

    The can post exercises to the mongoDB #unfinished

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
@@ -3,9 +3,7 @@
     var app = angular.module("userApp");
     
     var userDashboardController = function ($scope, userRestData, $routeParams) {
-        userRestData.getExercises.query(function (data) { 
-            $scope.exercises = data;
-        });
+        
     };
     app.controller("userDashboardController", userDashboardController);
 }());
--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userExercisesController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userExercisesController.js
@@ -1,16 +1,51 @@
 ﻿(function () {
     
     var app = angular.module("userApp");
+    var answer = {};
+    var newAnswer = {};
+    var userData;
     
-    var userExercisesController = function ($scope, userRestData, $routeParams) {
+    var userExercisesController = function ($scope, userRestData, $routeParams, $http) {
         userRestData.getExercises.query(function (data) {
             $scope.exercises = data;
         });
         
+        userRestData.getUser.get(function (data) {
+            $scope.userData = data;
+        });
+        
+
         $scope.select = function (id) {
             $scope.selected = $scope.exercises[id];
+            $scope.answers = $scope.exercises[id].questions;
+
+            answer.exerciseid = $scope.selected._id;
+            
+            
+            
+            answer.answers = $scope.answers;
+            $scope.answer = answer;
+
         }
 
+        $scope.processForm = function () {
+            
+            $http({
+                method  : 'POST',
+                url     : '/users/answer',
+                data    : $scope.answer,
+                responseType: 'text'
+            }).then(
+                //SUCCESS
+                function (response) {
+                    alert(response.data);
+                },
+                //ERROR
+                function (error) {
+                    alert(error.data);
+                }
+            );
+        };
     };
     
     app.controller("userExercisesController", userExercisesController);
