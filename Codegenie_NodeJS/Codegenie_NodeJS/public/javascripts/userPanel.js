46393fc110b1dc1752ecb5aa61c7611dc39a55cf
ErazerBrecht <brecht.carlier@student.ap.be>
2015-12-02 16:25:51 +0100

    Updated userpanel New CSS New way of showing questions

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/userPanel.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/userPanel.js
@@ -1,4 +1,27 @@
-﻿(function () {
+﻿//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
+$(function () {
+    $(window).bind("load resize", function () {
+        var topOffset = 50;
+        width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-collapse').removeClass('collapse');
+        }
+
+        var height = document.documentElement.clientHeight - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    });
+});
+
+(function () {
     
     var userApp = angular.module('userApp', ['ngRoute', 'ngResource', 'ui.ace']);
     
@@ -16,63 +39,3 @@
             .otherwise({ redirectTo: "/" });
     });
 }());
-
-
-
-/*$(function () {
-    populatePage();
-});
-
-function populatePage() {
-
-    var content = '';
-
-    // jQuery AJAX call for JSON
-    $.getJSON('/users/exercises', function (data) {
-        content += '<div class="row">';
-        // For each item in our JSON, add a div and a code editor
-        $.each(data, function () {
-            content += '<div onclick="getExercise(\'' + this._id + '\')" class="col-sm-6 col-md-4 col-lg-3" ><div class="tile blue"><div class="title"><h3>' + this.title + '</h1></div></div></div>';
-        });
-
-        // Inject the whole content string into our existing HTML 
-        $('#buttonList').html(content);
-    });
-};
-
-function initEditors() {
-    var editor;
-    $('.editor').each(function (index) {
-        editor = ace.edit(this);
-        editor.setTheme("ace/theme/sqlserver");
-        editor.getSession().setMode("ace/mode/csharp");
-    });
-}
-
-function getExercise(id) {
-    var content = '';
-
-    // jQuery AJAX call for JSON
-    $.getJSON('/users/exercises/' + id, function (data) {
-
-        // For each item in our JSON, add a div and a code editor
-        $.each(data, function () {
-            $.each(this.questions, function () {
-                if (this.extra)
-                    content += '<div> <span style="color:red; font-weight:bolder">EXTRA: </span>' + this.question + '</div>';
-                else
-                    content += '<div>' + this.question + '</div>';
-
-                switch (this.type) {
-                    case 'Checkbox': content += '<input type="checkbox">'; break;
-                    case 'Question': content += '<div contenteditable="true" class="answer"></div>'; break;
-                    case 'Code': content += '<div class="editor"></div>'; break;
-                }
-            });
-        });
-
-        // Inject the whole content string into our existing HTML 
-        $('#questionList').html(content);
-        initEditors();
-    });
-}*/
