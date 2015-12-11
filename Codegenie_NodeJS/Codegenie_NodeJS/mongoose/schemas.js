ccf66ad5a34a67bc02dad9e7637263d5a11708f2
Matthew Berkvens <matthew@berkvens.com>
2015-12-11 20:49:55 +0100

    Changed id references to be of the type ObjectId instead of String for correctness' sake

--- Codegenie_NodeJS/Codegenie_NodeJS/mongoose/schemas.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/mongoose/schemas.js
@@ -26,20 +26,20 @@ var exerciseSchema = mongoose.Schema({
     created: { type: Date, default: new Date().toISOString() },
     extra: { type: Boolean, default: false },
     questions: [{
-            questiontitle: { type: String, required: true },
-            weight: { type: Number, required: true },
-            extra: { type: Boolean, default: false },
-            type: { type: String, required: true, enum: typeEnum },
-            choices: [{
-                    text: String,
-                    correct: { type: Boolean, default: false }
-                }]
+        questiontitle: { type: String, required: true },
+        weight: { type: Number, required: true },
+        extra: { type: Boolean, default: false },
+        type: { type: String, required: true, enum: typeEnum },
+        choices: [{
+            text: String,
+            correct: { type: Boolean, default: false }
         }]
+    }]
 });
 
 var answerSchema = mongoose.Schema({
-    exerciseid: { type: String, required: true },
-    userid: { type: String, required: true },
+    exerciseid: { type: mongoose.Schema.ObjectId, required: true },
+    userid: { type: mongoose.Schema.ObjectId, required: true },
     title: { type: String, required: true },
     classification: { type: String, required: true },
     class: { type: String, required: true },
@@ -48,15 +48,15 @@ var answerSchema = mongoose.Schema({
     revised: { type: Boolean, default: false },
     created: { type: Date, default: new Date().toISOString() },
     answers: [{
-            questionid: { type: String, required: true },
-            questiontitle: { type: String, required: true },
-            received: { type: Number, default: 0 },
-            weight: { type: Number, required: true },
-            extra: { type: Boolean, required: true },
-            type: { type: String, required: true, enum: typeEnum },
-            result: {type: String},
-            choices: [{ text: String }]
-        }]
+        questionid: { type: mongoose.Schema.ObjectId, required: true },
+        questiontitle: { type: String, required: true },
+        received: { type: Number, default: 0 },
+        weight: { type: Number, required: true },
+        extra: { type: Boolean, required: true },
+        type: { type: String, required: true, enum: typeEnum },
+        result: { type: String },
+        choices: [{ text: String }]
+    }]
 });
 
 var UserModel = mongoose.model('User', userSchema);
