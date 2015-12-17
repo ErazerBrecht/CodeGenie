2ef67a98adf052d8cf5380a44b35885022baa354
ErazerBrecht <brecht.carlier@student.ap.be>
2015-12-17 21:52:42 +0100

    Implemented adminUsers view - Able to load Users - Able to show Users - Able to selected Users that need to be assigned to other course

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/adminUsersController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/adminUsersController.js
@@ -3,6 +3,36 @@
     var app = angular.module("adminApp");
 
     var adminUsersController = function ($scope, restData, $routeParams) {
+        restData.getAllUsers.query(function (data) {
+            $scope.users = data;
+            $scope.assign = {};
+            $scope.assign.users = [];
+        });
+
+        $scope.checkboxUser =  function (id)
+        {
+            if ($scope.users[id].checkbox)
+            {
+                $scope.assign.users.push($scope.users[id]._id);
+            }
+            else
+            {
+                 var index = $scope.assign.users.indexOf($scope.users[id]._id);
+                 if (index > -1)
+                 {
+                     $scope.assign.users.splice(index, 1);
+                 }
+            }
+        };
+
+        $scope.cancel = function () {
+            angular.forEach($scope.users, function (value, key) {
+                value.checkbox = false;
+            });
+
+            $scope.assign = {};
+            $scope.assign.users = [];
+        }
 
     };
 
