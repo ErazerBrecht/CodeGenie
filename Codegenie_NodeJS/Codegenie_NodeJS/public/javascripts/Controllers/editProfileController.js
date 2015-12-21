62cf3448b6d60d0e3398aeee9f5fc11a9c54dc82
ArneSchoonvliet <schoonvlietarne3@gmail.com>
2015-12-21 09:48:03 +0100

    Multiple choice answer can now been seen. Color is still subject to change

---
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/editProfileController.js
@@ -0,0 +1,13 @@
+(function () {
+
+   /* var adminApp = angular.module("adminApp");
+    var userApp = angular.module("userApp");
+
+    var editProfileController = function ($scope, $routeParams) {
+
+    };
+
+    adminApp.controller("editProfileController", editProfileController);
+    userApp.controller("editProfileController", editProfileController);*/
+
+}());
