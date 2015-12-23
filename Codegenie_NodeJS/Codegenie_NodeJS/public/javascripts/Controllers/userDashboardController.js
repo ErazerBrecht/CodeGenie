ae0917b28db53785f13d33d23be627ec8f198516
ErazerBrecht <brecht.carlier@student.ap.be>
2015-12-23 03:12:54 +0100

    Added real data in graph from REST API => At the moment bar graph => Line and area graph aren't working...

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
@@ -18,22 +18,13 @@
             });
         });
 
-        $scope.xkey = 'y';
+        userRestData.getStatsticsAnswersGraph.query(function (data)
+        {
+           $scope.graphAnswers = data;
+        });
 
-        $scope.ykeys = ["a", "b"];
-
-        $scope.labels = ["Series A", "Series B"];
-
-        $scope.myModel = [
-            { y: "2006", a: 100, b: 90 },
-            { y: "2007", a: 75,  b: 65 },
-            { y: "2008", a: 50,  b: 40 },
-            { y: "2009", a: 75,  b: 65 },
-            { y: "2010", a: 50,  b: 40 },
-            { y: "2011", a: 75,  b: 65 },
-            { y: "2012", a: 100, b: 90 }
-        ];
-
+        $scope.ykeys = ["count"];
+        $scope.labels = ["Answers"];
     };
 
     app.controller("userDashboardController", userDashboardController);
