d74275242722004f6e4ab1e05794c06401b111bd
ArneSchoonvliet <schoonvlietarne3@gmail.com>
2015-12-23 11:17:06 +0100

    Tiles of answers + delete answer Next stop correct answer and submit it

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/answersController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/answersController.js
@@ -3,7 +3,7 @@
     var app = angular.module("adminApp");
     
     
-    var answersController = function ($scope, restData, $routeParams) {
+    var answersController = function ($scope, restData, $routeParams, $http) {
         restData.getAllAnswers.query(function (data) {
             $scope.answers = data;
             $scope.addUserNames();
@@ -16,6 +16,85 @@
                 });
 
             });
+        };
+
+        $scope.select = function (id) {
+            $scope.selected = $scope.answers[id];
+            //$scope.selected.deadline = new Date($scope.selected.deadline);
+            $scope.error = null;
+            $scope.message = null;
+        };
+
+        /*$scope.processForm = function () {
+            //Clear error and message
+            $scope.error = null;
+            $scope.message = null;
+
+            $scope.selected.exerciseid = $scope.selected._id;
+
+            $http({
+                method  : 'POST',
+                url     : '/users/answer/',
+                data    : $scope.selected,
+                responseType: 'text'
+            }).then(
+                //SUCCESS
+                function (response) {
+                    $scope.message = response.data;
+                },
+                //ERROR
+                function (error) {
+                    $scope.error = error.data;
+                }
+            );
+
+        };*/
+
+        $scope.centerAnchor = true;
+        $scope.toggleCenterAnchor = function () {
+            $scope.centerAnchor = !$scope.centerAnchor
+        }
+
+        $scope.onDropComplete = function (data) {
+            var id = $scope.answers.indexOf(data);
+            $scope.DeleteExercise($scope.answers[id]);
+            $scope.answers.splice(id, 1);
+            $scope.selected = null;
+        };
+
+        //AJAX Call Delete
+        $scope.DeleteExercise = function (deletedExercise) {
+            $scope.message = null;
+            $scope.error = null;
+
+            $http({
+                method: 'GET',
+                url: '/admin/answers/delete/' + deletedExercise._id,
+                data: deletedExercise,
+                responseType: 'text'
+            }).then(
+                //SUCCESS
+                function (response) {
+                    $scope.message = response.data;
+                },
+                //ERROR
+                function (error) {
+                    //Scroll to top to show error
+                    $anchorScroll();
+                    $scope.error = error.data;
+                }
+            );
+        };
+
+        $scope.getTileClass = function (id) {
+            //Green when Answers is resolved, red for unresolved
+            if ($scope.answers[id].resolved) {
+                return "green";
+            }
+
+            return "red";
+
+            //TODO Add gray for invisible exercises
         };
 
     }
