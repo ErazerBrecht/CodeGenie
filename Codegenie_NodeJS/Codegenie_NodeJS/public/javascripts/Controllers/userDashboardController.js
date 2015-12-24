8106a4f6b8d6c05eea983e1f27a558051116b810
ErazerBrecht <brecht.carlier@student.ap.be>
2015-12-24 15:44:56 +0100

    Fixed small bug in userdashboard => Default show zero => if wasn't correct

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
@@ -7,11 +7,15 @@
         userRestData.getUser.get(function (data) {
             $scope.user = data;
             userRestData.getStatisticsAnswers.get(function (data) {
+                $scope.totalAnswersCourse = 0;
+                $scope.totalAnswers = 0;
+                $scope.myAnswers = 0;
+
                 $scope.totalAnswers = data.count;
                 $scope.myAnswers = data.myself;
                 angular.forEach(data.courses, function(value, key)
                 {
-                    if(value.course = $scope.user.course)
+                    if(value.course === $scope.user.course)
                         $scope.totalAnswersCourse = value.count;
 
                 });
