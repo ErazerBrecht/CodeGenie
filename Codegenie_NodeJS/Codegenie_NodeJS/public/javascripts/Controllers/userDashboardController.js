a9cceebf72ed5a54f70f5e7f75c3634a83159208
ErazerBrecht <brecht.carlier@student.ap.be>
2016-01-04 19:20:17 +0100

    Added panel to show activity in function of average activity

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
@@ -5,6 +5,8 @@
     var userDashboardController = function ($scope, userRestData, $routeParams) {
         userRestData.getStatisticsMyAnswersGraphWeek.get(function (data)
         {
+            $scope.logins = data.logins.mylogins;
+            $scope.loginsAverage = data.logins.average;
             $scope.myAnswers = data.received.length;
             $scope.graphAnswers = data.activity;
         });
