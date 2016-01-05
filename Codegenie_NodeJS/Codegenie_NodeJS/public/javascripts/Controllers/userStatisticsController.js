2656504fc9136f92fd98d3ead69edd1ed9a19562
ErazerBrecht <brecht.carlier@student.ap.be>
2016-01-05 11:59:02 +0100

    Changed to multibarchar, changed options for our needs => Use our data but still hardcoded

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userStatisticsController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userStatisticsController.js
@@ -27,97 +27,81 @@
         });
 
         $scope.options = {
-            chart: {
-                type: 'lineChart',
-                height: 450,
-                margin : {
-                    top: 20,
-                    right: 20,
-                    bottom: 40,
-                    left: 55
-                },
-                x: function(d){ return d.x; },
-                y: function(d){ return d.y; },
-                useInteractiveGuideline: true,
-                dispatch: {
-                    stateChange: function(e){ console.log("stateChange"); },
-                    changeState: function(e){ console.log("changeState"); },
-                    tooltipShow: function(e){ console.log("tooltipShow"); },
-                    tooltipHide: function(e){ console.log("tooltipHide"); }
-                },
-                xAxis: {
-                    axisLabel: 'Time (ms)'
-                },
-                yAxis: {
-                    axisLabel: 'Voltage (v)',
-                    tickFormat: function(d){
-                        return d3.format('.02f')(d);
-                    },
-                    axisLabelDistance: -10
-                },
-                callback: function(chart){
-                    console.log("!!! lineChart callback !!!");
-                }
+            "chart": {
+            "type": "multiBarChart",
+                "height": 450,
+                "margin": {
+                "top": 20,
+                    "right": 20,
+                    "bottom": 40,
+                    "left": 55
             },
-            title: {
-                enable: true,
-                text: 'Title for Line Chart'
+            "useInteractiveGuideline": true,
+                "dispatch": {},
+            "xAxis": {
+                "axisLabel": "Week"
             },
-            subtitle: {
-                enable: true,
-                text: 'Subtitle for simple line chart. Lorem ipsum dolor sit amet, at eam blandit sadipscing, vim adhuc sanctus disputando ex, cu usu affert alienum urbanitas.',
-                css: {
-                    'text-align': 'center',
-                    'margin': '10px 13px 0px 7px'
-                }
-            },
-            caption: {
-                enable: true,
-                html: '<b>Figure 1.</b> Lorem ipsum dolor sit amet, at eam blandit sadipscing, <span style="text-decoration: underline;">vim adhuc sanctus disputando ex</span>, cu usu affert alienum urbanitas. <i>Cum in purto erat, mea ne nominavi persecuti reformidans.</i> Docendi blandit abhorreant ea has, minim tantas alterum pro eu. <span style="color: darkred;">Exerci graeci ad vix, elit tacimates ea duo</span>. Id mel eruditi fuisset. Stet vidit patrioque in pro, eum ex veri verterem abhorreant, id unum oportere intellegam nec<sup>[1, <a href="https://github.com/krispo/angular-nvd3" target="_blank">2</a>, 3]</sup>.',
-                css: {
-                    'text-align': 'justify',
-                    'margin': '10px 13px 0px 7px'
-                }
+            "yAxis": {
+                "axisLabel": "Exercises",
+                    "axisLabelDistance": -10
             }
+        },
+            "title": {
+            "enable": false,
+                "text": null
+        },
+            "subtitle": {
+            "enable": false,
+                "text": null,
+                "css": {
+                "text-align": "center",
+                    "margin": "10px 13px 0px 7px"
+            }
+        },
+            "caption": {
+            "enable": false,
+                "html": null,
+                "css": {
+                "text-align": "justify",
+                    "margin": "10px 13px 0px 7px"
+            }
+        }
         };
 
-        $scope.data = sinAndCos();
+        $scope.data = GetGraphData();
 
-        /*Random Data Generator */
-        function sinAndCos() {
-            var sin = [],sin2 = [],
-                cos = [];
+        function GetGraphData() {
 
-            //Data is represented as an array of {x,y} pairs.
-            for (var i = 0; i < 100; i++) {
-                sin.push({x: i, y: Math.sin(i/10)});
-                sin2.push({x: i, y: i % 10 == 5 ? null : Math.sin(i/10) *0.25 + 0.5});
-                cos.push({x: i, y: .5 * Math.cos(i/10+ 2) + Math.random() / 10});
-            }
+            //$scope.graphAnswers = data;
+
+            var data = [
+                {
+                    x: 50,
+                    y: 4
+                },
+                {
+                    x: 51,
+                    y: 7
+                },
+                {
+                    x: 52,
+                    y: 0
+                },
+                {
+                    x: 1,
+                    y: 12
+                }
+            ]
 
             //Line chart data should be sent as an array of series objects.
             return [
                 {
-                    values: sin,      //values - represents the array of {x,y} data points
-                    key: 'Sine Wave', //key  - the name of the series.
-                    color: '#ff7f0e',  //color - optional: choose your own line color.
-                    strokeWidth: 2,
-                    classed: 'dashed'
-                },
-                {
-                    values: cos,
-                    key: 'Cosine Wave',
-                    color: '#2ca02c'
-                },
-                {
-                    values: sin2,
-                    key: 'Another sine wave',
-                    color: '#7777ff',
-                    area: true      //area - set to true if you want this line to turn into a filled area chart.
+                    values: data,      //values - represents the array of {x,y} data points
+                    key: 'Total', //key  - the name of the series.
+                    color: '#337ab7',  //color - optional: choose your own line color.
                 }
             ];
-        };
-
+        }
     };
 
     app.controller("userStatisticsController", userStatisticsController);
