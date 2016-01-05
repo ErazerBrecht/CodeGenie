aff2b91487a3c3ed248f1c97f7a55671cb998183
ErazerBrecht <brecht.carlier@student.ap.be>
2016-01-05 08:11:01 +0100

    Front end of punch card finished

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
@@ -13,7 +13,6 @@
 
         userRestData.getStatisticsMyAnswersGraphHour.get(function (data)
         {
-            //ES6 code, prob not supported in old browsers....
             $scope.punchCardData = [
                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
             ];
@@ -25,10 +24,6 @@
 
         $scope.ykeys = ["total"];
         $scope.labels = ["Answers"];
-
-        $scope.punchCardData = [
-            [12, 17, 10, 20, 0, 12, 12, 12, 12, 20, 12, 10,0,0,0,0,0,0,0,0,0,0,0,37]
-        ];
     };
 
     app.controller("userDashboardController", userDashboardController);
