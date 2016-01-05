2dc82e3fca1d7612a200cc1313c6527097b84cc4
ErazerBrecht <brecht.carlier@student.ap.be>
2016-01-05 23:34:20 +0100

    Also added new graph @ UserDashboard

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userDashboardController.js
@@ -8,11 +8,20 @@
             $scope.logins = data.logins.mylogins;
             $scope.loginsAverage = data.logins.average;
             $scope.myAnswers = data.received.length;
-            $scope.graphAnswers = data.activity;
+
+            //Chart data should be sent as an array of series objects.
+            $scope.graphData = [
+                {
+                    values: data.activity,      //values - represents the array of {x,y} data points
+                    key: 'Your answers', //key  - the name of the series.
+                    color: '#5cb85c',  //color - optional: choose your own line color.
+                }
+            ];
         });
 
         userRestData.getStatisticsMyAnswersGraphHour.get(function (data)
         {
+            //This needs to be a 2 dimensional array!!
             $scope.punchCardData = [
                 [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
             ];
@@ -22,8 +31,47 @@
             });
         });
 
-        $scope.ykeys = ["total"];
-        $scope.labels = ["Answers"];
+        $scope.options = {
+            "chart": {
+                "type": "multiBarChart",
+                "height": 450,
+                "margin": {
+                    "top": 20,
+                    "right": 20,
+                    "bottom": 40,
+                    "left": 55
+                },
+                "useInteractiveGuideline": true,
+                "dispatch": {},
+                "xAxis": {
+                    "axisLabel": "Week"
+                },
+                "yAxis": {
+                    "axisLabel": "Exercises",
+                    "axisLabelDistance": -10
+                }
+            },
+            "title": {
+                "enable": false,
+                "text": null
+            },
+            "subtitle": {
+                "enable": false,
+                "text": null,
+                "css": {
+                    "text-align": "center",
+                    "margin": "10px 13px 0px 7px"
+                }
+            },
+            "caption": {
+                "enable": false,
+                "html": null,
+                "css": {
+                    "text-align": "justify",
+                    "margin": "10px 13px 0px 7px"
+                }
+            }
+        };
     };
 
     app.controller("userDashboardController", userDashboardController);
--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userStatisticsController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/Controllers/userStatisticsController.js
@@ -1,7 +1,7 @@
 ﻿(function () {
-    
+
     var app = angular.module("userApp");
-    
+
     var userStatisticsController = function ($scope, userRestData, $routeParams) {
         userRestData.getUser.get(function (data) {
             $scope.user = data;
@@ -12,98 +12,92 @@
 
                 $scope.totalAnswers = data.count;
                 $scope.myAnswers = data.myself;
-                angular.forEach(data.courses, function(value, key)
-                {
-                    if(value.course === $scope.user.course)
+                angular.forEach(data.courses, function (value, key) {
+                    if (value.course === $scope.user.course)
                         $scope.totalAnswersCourse = value.count;
 
                 });
             });
         });
 
-        userRestData.getStatisticsAnswersGraph.query(function (data)
-        {
-           $scope.graphAnswers = data;
-        });
-
         $scope.options = {
             "chart": {
-            "type": "multiBarChart",
+                "type": "multiBarChart",
                 "height": 450,
                 "margin": {
-                "top": 20,
+                    "top": 20,
                     "right": 20,
                     "bottom": 40,
                     "left": 55
-            },
-            "useInteractiveGuideline": true,
+                },
+                "useInteractiveGuideline": true,
                 "dispatch": {},
-            "xAxis": {
-                "axisLabel": "Week"
-            },
-            "yAxis": {
-                "axisLabel": "Exercises",
+                "xAxis": {
+                    "axisLabel": "Week"
+                },
+                "yAxis": {
+                    "axisLabel": "Exercises",
                     "axisLabelDistance": -10
-            }
-        },
+                }
+            },
             "title": {
-            "enable": false,
+                "enable": false,
                 "text": null
-        },
+            },
             "subtitle": {
-            "enable": false,
+                "enable": false,
                 "text": null,
                 "css": {
-                "text-align": "center",
+                    "text-align": "center",
                     "margin": "10px 13px 0px 7px"
-            }
-        },
+                }
+            },
             "caption": {
-            "enable": false,
+                "enable": false,
                 "html": null,
                 "css": {
-                "text-align": "justify",
+                    "text-align": "justify",
                     "margin": "10px 13px 0px 7px"
+                }
             }
-        }
         };
 
-            userRestData.getStatisticsAnswersGraph.query(function (data)
-            {
-                var Hardcoded = [
-                    {
-                        x: 50,
-                        y: 9
-                    },
-                    {
-                        x: 51,
-                        y: 10
-                    },
-                    {
-                        x: 52,
-                        y: 5
-                    },
-                    {
-                        x: 1,
-                        y: 2
-                    }
-                ]
+        userRestData.getStatisticsAnswersGraph.query(function (data) {
+            //For testing purposes
+            var Hardcoded = [
+                {
+                    x: 50,
+                    y: 9
+                },
+                {
+                    x: 51,
+                    y: 10
+                },
+                {
+                    x: 52,
+                    y: 5
+                },
+                {
+                    x: 1,
+                    y: 2
+                }
+            ]
 
-                //Line chart data should be sent as an array of series objects.
-                $scope.graphData = [
-                    {
-                        values: data,      //values - represents the array of {x,y} data points
-                        key: 'Total', //key  - the name of the series.
-                        color: '#337ab7',  //color - optional: choose your own line color.
-                    },
-                    {
-                        values: Hardcoded,      //values - represents the array of {x,y} data points
-                        key: 'Hardcoded ', //key  - the name of the series.
-                        color: '#f0ad4e',  //color - optional: choose your own line color.
-                    }
-                ];
-            });
-        };
+            //Chart data should be sent as an array of series objects.
+            $scope.graphData = [
+                {
+                    values: data,      //values - represents the array of {x,y} data points
+                    key: 'Total', //key  - the name of the series.
+                    color: '#337ab7',  //color - optional: choose your own line color.
+                },
+                {
+                    values: Hardcoded,      //values - represents the array of {x,y} data points
+                    key: 'Hardcoded ', //key  - the name of the series.
+                    color: '#f0ad4e',  //color - optional: choose your own line color.
+                }
+            ];
+        });
+    };
 
     app.controller("userStatisticsController", userStatisticsController);
 }());
