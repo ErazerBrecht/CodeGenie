03b501b8a0b09fd57edac5b12530ed33ef9438ff
ErazerBrecht <brecht.carlier@student.ap.be>
2016-01-10 00:09:01 +0100

    Added a favicon Finally done with those wannabe errors

--- Codegenie_NodeJS/Codegenie_NodeJS/server.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/server.js
@@ -50,6 +50,7 @@ app.set('view engine', 'jade');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 app.use(flash());
 app.use(session({
     secret: "shhhhhitsasecret",
