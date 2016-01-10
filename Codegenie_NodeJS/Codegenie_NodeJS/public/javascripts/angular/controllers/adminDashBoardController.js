b37a4271515eb6778e8d2dc593c0c682a094337e
ErazerBrecht <brecht.carlier@student.ap.be>
2016-01-10 20:33:31 +0100

    Fixed graph also in admin dashboard Added average.

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/angular/controllers/adminDashBoardController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/angular/controllers/adminDashBoardController.js
@@ -28,58 +28,70 @@
             $scope.answerStatistics = data.courses;
         });
 
-        restData.getStatisticsAnswersGraph.query(function (data)
+        restData.getStatisticsAnswersGraph.get(function (data)
         {
+            var max = 0;
+
+            for(var i = 0; i < data.graphWeekly.length; i++)
+            {
+                if(data.graphWeekly[i].y > max)
+                    max = data.graphWeekly[i].y;
+            }
+
+            $scope.options = {
+                chart: {
+                    type: 'multiChart',
+                    height: 450,
+                    margin : {
+                        top: 30,
+                        right: 60,
+                        bottom: 50,
+                        left: 70
+                    },
+                    yDomain1: [0, max],
+                    yDomain2: [0, 100],
+                    color: d3.scale.category10().range(),
+                    //useInteractiveGuideline: true,
+                    duration: 500,
+                    xAxis: {
+                        "axisLabel": "Week",
+                        tickFormat: function(d){
+                            return d3.format(',f')(d);
+                        }
+                    },
+                    yAxis1: {
+                        "axisLabel": "Exercises",
+                        tickFormat: function(d){
+                            return d3.format(',.f')(d);
+                        }
+                    },
+                    yAxis2: {
+                        "axisLabel": "Percent",
+                        tickFormat: function(d){
+                            return d3.format(',.f')(d);
+                        }
+                    },
+                }
+            };
+
+            //Chart data should be sent as an array of series objects.
             $scope.graphData = [
                 {
-                    values: data,      //values - represents the array of {x,y} data points
+                    values: data.graphWeekly,      //values - represents the array of {x,y} data points
                     key: 'Total answers', //key  - the name of the series.
                     color: '#337ab7',  //color - optional: choose your own line color.
+                    type: "bar",
+                    yAxis: 1
+                },
+                {
+                    values: data.graphAverageWeekly,      //values - represents the array of {x,y} data points
+                    key: 'Total average', //key  - the name of the series.
+                    color: '#f0ad4e',  //color -
+                    type: "line",
+                    yAxis: 2
                 }
             ];
         });
-
-        $scope.options = {
-            "chart": {
-                "type": "multiBarChart",
-                "height": 450,
-                "margin": {
-                    "top": 20,
-                    "right": 20,
-                    "bottom": 40,
-                    "left": 55
-                },
-                "useInteractiveGuideline": true,
-                "dispatch": {},
-                "xAxis": {
-                    "axisLabel": "Week"
-                },
-                "yAxis": {
-                    "axisLabel": "Exercises",
-                    "axisLabelDistance": -10
-                }
-            },
-            "title": {
-                "enable": false,
-                "text": null
-            },
-            "subtitle": {
-                "enable": false,
-                "text": null,
-                "css": {
-                    "text-align": "center",
-                    "margin": "10px 13px 0px 7px"
-                }
-            },
-            "caption": {
-                "enable": false,
-                "html": null,
-                "css": {
-                    "text-align": "justify",
-                    "margin": "10px 13px 0px 7px"
-                }
-            }
-        };
 
         $scope.getClass = function(statistic)
         {
