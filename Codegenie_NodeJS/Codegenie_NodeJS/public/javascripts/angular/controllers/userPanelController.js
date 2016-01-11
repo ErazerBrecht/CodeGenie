dc047feaed13fe48603e280a6279d2fcf7e80c52
ErazerBrecht <brecht.carlier@student.ap.be>
2016-01-11 01:14:53 +0100

    Added comments in userPanelController.js

--- Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/angular/controllers/userPanelController.js
+++ Codegenie_NodeJS/Codegenie_NodeJS/public/javascripts/angular/controllers/userPanelController.js
@@ -3,11 +3,25 @@
     var app = angular.module("userApp");
     
     var userPanelController = function ($scope, userRestData) {
-        
+
+        //Get user data like username, e-mail, course
+        //Username is used in the top - right corner of our frontend (userpanel.jade)
         userRestData.getUser.get(function (data) {
             $scope.user = data;
         });
 
+        //Get all exercises...
+        //Why now?
+        //The chance that the user will visit the exercises view is big
+        //Otherwise there is not much reason for his login... (maybe he likes punch cards)
+        //We wont reload this data when the user open the exercises
+        //We will just use this data!
+        //This controller is a parent controller of userExercisesController
+        //So we are able to get this data without connection to the server again!
+        //Why need it here?
+        //That's for our notifications, we want to show unseen en unseen revised exercises
+        //Previously we used a separate route, but than we loaded the data double.
+        //This isn't the case anymore (explanation above)
         userRestData.getExercises.query(function (data) {
             $scope.exercises = data;
             $scope.newExercises = 0;
@@ -18,23 +32,32 @@
                 value.deadline = new Date(value.deadline);
                 value.revised = false;
 
+                //Calculate amount of unseen exercises
                 if(!value.seen)
                     $scope.newExercises += 1;
 
+                //If value is solved immediately add the corresponding answer object
+                //The user will be able to check his answers
                 if (value.solved) {
                     userRestData.getAnswer.get({exerciseid: value._id}, function (data) {
                         value.answers = data.answers;
+                        //Convert date to a real date type!
                         value.answerDate = new Date(data.created);
                         value.revised = data.revised;
                         value.totalpoints = 0;
                         value.totalAvailablePoints = 0;
+
+                        //If the answer is revised, calculated the total received points and the max points
+                        //Example: 15/20 => 15 received points, 20 maxpoints (availablepoints)
                         if(value.revised) {
                             angular.forEach(value.answers, function (d, key) {
                                 value.totalpoints += d.received;
                                 value.totalAvailablePoints += d.weight;
+                                //TODO: Add date when answer was revised (needs to be added in back-end)
                             });
                         }
 
+                        //Calculate the amount of revised answers the user didn't check
                         if(!value.revisedseen)
                         $scope.newRevised += 1;
                     });
@@ -42,6 +65,7 @@
             });
         });
 
+        //Function to discard the notification of an unseen exercise
         $scope.discard = function(exercise)
         {
             //Change it in our DB
@@ -49,7 +73,7 @@
 
             });
 
-            //Also change this in local data
+            //Also change this in local data (no need for reload than)
             var index = $scope.exercises.indexOf(exercise);
             if (index > -1)
             {
@@ -59,6 +83,7 @@
             $scope.newExercises--;
         };
 
+        //Function to discard the notification of an unseen revised exercise
         $scope.discardRevised = function(exercise)
         {
             //Change it in our DB
@@ -66,7 +91,7 @@
 
             });
 
-            //Also change this in local data
+            //Also change this in local data (no need for reload than)
             var index = $scope.exercises.indexOf(exercise);
             if (index > -1)
             {
